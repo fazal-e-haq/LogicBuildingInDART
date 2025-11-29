2dd9335fcaff7b29664d9b012ad98315952df2f9
fazal-e-haq <fazalehaq37405@GMAIL.COM>
2025-11-29 21:47:15 +0500

    problem solved

--- Day1.dart
+++ Day1.dart
@@ -4,7 +4,19 @@ List num=[1,5,3,1,5,9,8];
 
 print('before sorting $num');
 
-num.sort();
-print('after sorting $num');
+ for(int i=0;i<num.length;i++){
+    for(int j=i+1;j<num.length;j++){
+       if(num[i]>num[j]){
+    int temp =num[i];
+    num[i]=num[j];
+    num[j]=temp;
+}
+ 
+    }
+      
+
+ 
+ }
+ print(num);
 
 }
